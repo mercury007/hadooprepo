f15fc416ade77ada58a9562c5fcc6bf3fd78b70a
mercury007 <prashanth_ravikumar@yahoo.com>
2013-05-02 23:17:31 -0400

    MRunit Test

---
+++ mapreduce-jobs/src/main/java/com/collective/hadoopcoding/HadoopQuotesTest.java
@@ -0,0 +1,15 @@
+package com.collective.hadoopcoding;
+
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HadoopQuotesTest {
+
+}
